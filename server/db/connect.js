67fd2e7bd90a6dbe39869a9ec31ae7415b6f248d
NickyY28 <nicky28bt@gmail.com>
2026-03-28 08:26:54 +0530

    refactor: enhance comments for clarity on async/await usage in MongoDB connection

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // mongoose is a tool that helps communication b/w nodejs server and mongodb
 
 const connectDB = async () => {
   try {
@@ -12,4 +12,5 @@ const connectDB = async () => {
 
 module.exports = connectDB;
 
-// why we used async await here? cause when we connect 
+// why we used async await here? cause when we connect it takes time to connect so we use async await to wait for the connection to complete meanwhile other code can run and application doesn't freeze
+// what is the purpose of await? await is used to wait for the promise to resolve and return the value of the promise if we don't use it then js won't wait for the connection to complete and will move on to the next line and hence give error before getting connected to the db
